8d802c3606aa462d50fb151c7c74e48d659cb67f
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-04-23 20:44:21 +0100

    Implement update/delete form.

--- client/templates/locations/location_edit.js
+++ client/templates/locations/location_edit.js
@@ -1,42 +1,4 @@
-Template.locationEdit.created = function() {
-  Session.set('locationEditErrors', {});
-}
-
-Template.locationEdit.helpers({
-  errorMessage: function(field) {
-    return Session.get('locationEditErrors')[field];
-  },
-  errorClass: function (field) {
-    return !!Session.get('locationEditErrors')[field] ? 'has-error' : '';
-  }
-});
-
 Template.locationEdit.events({
-  'submit form': function(e) {
-    e.preventDefault();
-
-    var currentLocationId = this._id;
-
-    var locationProperties = {
-      url: $(e.target).find('[name=url]').val(),
-      description: $(e.target).find('[name=description]').val(),
-      title: $(e.target).find('[name=title]').val()
-    }
-
-    var errors = validateLocation(locationProperties);
-    if (errors.title || errors.url || errors.description)
-      return Session.set('locationEditErrors', errors);
-
-    Locations.update(currentLocationId, {$set: locationProperties}, function(error) {
-      if (error) {
-        // display the error to the user
-        throwError(error.reason);
-      } else {
-        Router.go('locationPage', {_id: currentLocationId});
-      }
-    });
-  },
-
   'click .delete': function(e) {
     e.preventDefault();
 
@@ -45,5 +7,13 @@ Template.locationEdit.events({
       Locations.remove(currentLocationId);
       Router.go('locationsList');
     }
+  }
+});
+
+AutoForm.hooks({
+  editLocationForm: {
+    onSuccess: function(formType, result) {
+      Router.go('locationPage', {_id: this.docId});
+    },
   }
 });
