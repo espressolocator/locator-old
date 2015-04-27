4893ece2da50872482a86d08effae39d51dc84da
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-04-27 22:14:12 +0100

    Refactor location_edit to use server method.

--- client/templates/locations/location_edit.js
+++ client/templates/locations/location_edit.js
@@ -12,8 +12,27 @@ Template.locationEdit.events({
 
 AutoForm.hooks({
   editLocationForm: {
-    onSuccess: function(formType, result) {
-      Router.go('locationPage', {_id: this.docId});
+    before: {
+      "method-update": function(doc) {
+        // Remove sticky validation errors if any.
+        if (this.validationContext.keyIsInvalid('url')) {
+          this.removeStickyValidationError('url');
+        }
+        return doc;
+      }
     },
+    onSuccess: function(formType, result) {
+      // Reset validation and form prior to redirection.
+      this.validationContext.resetValidation();
+      this.resetForm();
+      Router.go('locationPage', {_id: result._id});
+    },
+    onError: function(formType, error) {
+      // Process non-unique filed error, add sticky error.
+      if (error.error === 'urlnotunique') {
+        this.addStickyValidationError('url', 'notUnique');
+        AutoForm.validateField(this.formId, 'url');
+      }
+    }
   }
 });
