26ffaf06500d8902ba8480b1797164763e1799c0
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-04-27 19:54:18 +0100

    Fix flipping field error.
    For discusssion see https://github.com/aldeed/meteor-autoform/issues/769,
    https://github.com/aldeed/meteor-autoform/issues/668

--- client/templates/locations/location_submit.js
+++ client/templates/locations/location_submit.js
@@ -1,12 +1,25 @@
 AutoForm.hooks({
   insertLocationForm: {
+    before: {
+      method: function(doc) {
+        // Remove sticky validation errors if any.
+        if (this.validationContext.keyIsInvalid('url')) {
+          this.removeStickyValidationError('url');
+        }
+        return doc;
+      }
+    },
     onSuccess: function(formType, result) {
+      // Reset validation and form prior to redirection.
+      this.validationContext.resetValidation();
       this.resetForm();
       Router.go('locationPage', {_id: result._id});
     },
     onError: function(formType, error) {
+      // Process non-unique filed error, add sticky error.
       if (error.error === 'urlnotunique') {
-        this.validationContext.addInvalidKeys([{name: "url", type: "notUnique"}]);
+        this.addStickyValidationError('url', 'notUnique');
+        AutoForm.validateField(this.formId, 'url');
       }
     }
   }
