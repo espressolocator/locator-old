9ad60e5d290b1856b3542ee66a817c1201af896c
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-05-20 20:40:01 +0100

    Add comments notifications.

---
+++ client/templates/notifications/notifications.js
@@ -0,0 +1,20 @@
+Template.notifications.helpers({
+  notifications: function() {
+    return Notifications.find({userId: Meteor.userId(), read: false});
+  },
+  notificationCount: function(){
+    return Notifications.find({userId: Meteor.userId(), read: false}).count();
+  }
+});
+
+Template.notificationItem.helpers({
+  notificationLocationPath: function() {
+    return Router.routes.locationPage.path({_id: this.locationId});
+  }
+});
+
+Template.notificationItem.events({
+  'click a': function() {
+    Notifications.update(this._id, {$set: {read: true}});
+  }
+});
--- server/publications.js
+++ server/publications.js
@@ -6,3 +6,7 @@ Meteor.publish('comments', function(locationId) {
   check(locationId, String);
   return Comments.find({locationId: locationId});
 });
+
+Meteor.publish('notifications', function() {
+  return Notifications.find({userId: this.userId, read: false});
+});
