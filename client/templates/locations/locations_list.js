fb2a92e1cbb8681fc3f62236dabc17b46207039f
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-05-21 23:57:59 +0100

    Add map to the locations list page.

--- client/templates/locations/locations_list.js
+++ client/templates/locations/locations_list.js
@@ -3,3 +3,40 @@ Template.locationsList.helpers({
     return Locations.find({}, {sort: {submitted: -1}});
   }
 });
+
+Template.locationsList.onRendered(function() {
+  var self = this;
+  this.autorun(function(c) {
+    if (GoogleMaps.loaded()) {
+      var canvas = self.$('.map-container').get(0);
+      var mapOptions = {
+        zoom: 12,
+        scrollwheel: true,
+        streetViewControl: false
+      };
+      // Create map object.
+      var map = new google.maps.Map(canvas, mapOptions);
+      // Center to current location.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          currentLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+          map.setCenter(currentLocation);
+        });
+      }
+      // Also create GoogleMaps object for convenience.
+      GoogleMaps._create('locationsMap', {
+        type: 'Map',
+        instance: map,
+        options: mapOptions
+      });
+
+      c.stop();
+    }
+  });
+});
+
+Template.locationsList.onCreated(function() {
+  GoogleMaps.load({
+    libraries: 'places'
+  });
+});
