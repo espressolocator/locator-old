a8f24ba2903f2f9bd0b97d846274d99eb958a167
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-05-31 17:02:48 +0100

    Implement template-level subscription for locations list.

--- client/templates/locations/locations_list.js
+++ client/templates/locations/locations_list.js
@@ -1,5 +1,17 @@
 Template.locationsList.helpers({
   locations: function() {
+    return Template.instance().locations();
+  }
+});
+
+Template.locationsList.onCreated(function() {
+  var instance = this;
+  instance.autorun(function () {
+    // Subscribe to the locations publication.
+    var subscription = instance.subscribe('locations', Session.get('mapBounds'));
+  });
+  // Locations cursor.
+  instance.locations = function() {
     return Locations.find({}, {sort: {submitted: -1}});
   }
 });
