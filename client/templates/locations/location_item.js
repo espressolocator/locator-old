2218d3dec39536f4290ea96b3f02720ba2375933
Ruslan Kabalin <ruslan.kabalin@luns.net.uk>
2015-06-14 11:28:43 +0100

    Replace discuss button with edit.

--- client/templates/locations/location_item.js
+++ client/templates/locations/location_item.js
@@ -2,9 +2,4 @@ Template.locationItem.helpers({
   canEdit: function() {
     return ((this.createdBy.userId === Meteor.userId()) || Roles.userIsInRole(Meteor.user(), ['admin']));
   },
-  domain: function() {
-    var a = document.createElement('a');
-    a.href = this.url;
-    return a.hostname;
-  }
 });
