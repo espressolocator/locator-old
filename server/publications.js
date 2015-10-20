b86af29fbfadb95f216dfc9d3503c4053a66512b
Ruslan Kabalin <ruslan.kabalin@gmail.com>
2015-10-20 23:00:13 +0100

    Change sort order for comments on location page.
    And move comment form above comments.

--- server/publications.js
+++ server/publications.js
@@ -1,6 +1,6 @@
 Meteor.publish('comments', function(locationId) {
   check(locationId, String);
-  return Comments.find({locationId: locationId});
+  return Comments.find({locationId: locationId}, {sort: {createdAt: -1}});
 });
 
 Meteor.publish('notifications', function() {
